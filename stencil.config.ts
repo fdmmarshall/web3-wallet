17dfb229baf2105dd4067ae2cd1ac93de5e39a89
Flor Marshall <flordmarshall@gmail.com>
2022-01-15 12:27:43 -0500

    modify stencil config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,21 +1,53 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
+import { postcss } from "@stencil/postcss";
+import autoprefixer from "autoprefixer";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export const config: Config = {
-  namespace: 'stellar-wallet',
+  namespace: "stellar-wallet",
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
+      type: "dist",
+      esmLoaderPath: "../loader",
     },
     {
-      type: 'dist-custom-elements',
+      type: "docs-readme",
     },
     {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
+      type: "www",
       serviceWorker: null, // disable service workers
     },
   ],
+  globalStyle: "src/global/style.scss",
+  commonjs: {
+    namedExports: {
+      "stellar-sdk": [
+        "StrKey",
+        "xdr",
+        "Transaction",
+        "Keypair",
+        "Networks",
+        "Account",
+        "TransactionBuilder",
+        "BASE_FEE",
+        "Operation",
+        "Asset",
+        "Memo",
+        "MemoHash",
+      ],
+      "@stellar/wallet-sdk": ["KeyManager", "KeyManagerPlugins", "KeyType"],
+    },
+  },
+  plugins: [
+    nodePolyfills(),
+    sass(),
+    postcss({
+      plugins: [autoprefixer()],
+    }),
+  ],
+  nodeResolve: {
+    browser: true,
+    preferBuiltins: true,
+  },
 };
