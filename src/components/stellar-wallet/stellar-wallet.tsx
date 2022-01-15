c7f672a4253aa05849995eeb83d7911955743301
Flor Marshall <flordmarshall@gmail.com>
2022-01-15 12:38:13 -0500

    edit component

--- src/components/stellar-wallet/stellar-wallet.tsx
+++ src/components/stellar-wallet/stellar-wallet.tsx
@@ -1,18 +1,13 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h } from '@stencil/core';
+import * as StellarSdk from 'stellar-sdk';
 
 @Component({
   tag: 'stellar-wallet',
-  styleUrl: 'stellar-wallet.css',
+  styleUrl: 'stellar-wallet.scss',
   shadow: true,
 })
 export class StellarWallet {
-
   render() {
-    return (
-      <Host>
-        <slot></slot>
-      </Host>
-    );
+    return [<h1>{!!StellarSdk ? 'The StellarSdk is ready to rock 🤘' : 'Uh oh, the StellarSdk is missing 😱'}</h1>];
   }
-
 }
